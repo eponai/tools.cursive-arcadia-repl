7ba14e8d892c8cf26044633cae91ad1ac8755d97
Diana Negma <dianagren@gmail.com>
2019-05-14 21:25:08 -0700

    CR comments.

--- src/arcadia/nrepl.clj
+++ src/arcadia/nrepl.clj
@@ -8,25 +8,36 @@
     [nrepl.core :as nrepl]))
 
 
+(def ^:dynamic *nrepl-conn* "Transport for connection to Arcadia's nREPL." nil)
+
+
+(def cli-opts
+  [["-p" "--port PORT" "Port number for nREPL server that Cursive should connect to."
+    :default 7888
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-a" "--arcadia-port PORT" "Port number for Arcadia nREPL running in Unity."
+    :default 3722
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
+
+
 (defn from-cursive?
   "Returns true if expr is loaded by Cursive."
   [expr]
   (and (string? expr)
-       (re-find #"cursive.repl.runtime" expr)))
-
-
-(def ^:dynamic *nrepl* nil)
+    (re-find #"cursive.repl.runtime" expr)))
 
 
 (defn arcadia-nrepl-eval
   [handler {:keys [transport session] :as msg}]
-  (let [code (or (:code msg) (:file msg))
-        client (nrepl/client *nrepl* 1000)]
+  (let [code   (or (:code msg) (:file msg))
+        client (nrepl/client *nrepl-conn* 1000)]
     (if (from-cursive? code)
       (handler msg)
-      (let [id-id (:id msg)
-            msg (select-keys msg [:id :op :code :file :file-name :file-path])
-            res (nrepl/message client msg)
+      (let [id-id      (:id msg)
+            msg        (select-keys msg [:id :op :code :file :file-name :file-path])
+            res        (nrepl/message client msg)
 
             session-id (if (instance? clojure.lang.AReference session)
                          (-> session meta :id)
@@ -34,10 +45,10 @@
 
         (doseq [r res]
           (transport/send transport (cond-> r
-                                            (some? session-id)
-                                            (assoc :session session-id)
-                                            (some? id-id)
-                                            (assoc :id id-id))))))))
+                                      (some? session-id)
+                                      (assoc :session session-id)
+                                      (some? id-id)
+                                      (assoc :id id-id))))))))
 
 
 (defn wrap-arcadia-repl
@@ -49,27 +60,18 @@
 
 
 (set-descriptor! #'wrap-arcadia-repl
-                 {:requires #{"clone"}
-                  :expects #{"eval" "load-file"}
-                  :handles {}})
+  {:requires #{"clone"}
+   :expects  #{"eval" "load-file"}
+   :handles  {}})
 
 
 (defn -main
   [& args]
-  (let [cli-opts [["-p" "--port PORT" "Port number for nREPL server that Cursive should connect to."
-                   :default 7888
-                   :parse-fn #(Integer/parseInt %)
-                   :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-                  ["-a" "--arcadia-port PORT" "Port number for Arcadia nREPL running in Unity."
-                   :default 3722
-                   :parse-fn #(Integer/parseInt %)
-                   :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]]
-        {:keys [arcadia-port port]} (:options (cli/parse-opts args cli-opts))]
-
+  (let [{:keys [arcadia-port port]} (:options (cli/parse-opts args cli-opts))]
     (print (format "Connecting to Arcadia on port %d... " arcadia-port))
     (with-open [conn (nrepl/connect :port arcadia-port)]
       (println "Done!")
-      (binding [*nrepl* conn]
+      (binding [*nrepl-conn* conn]
         (print (format "Starting nREPL server on port %d... " port))
         (server/start-server
           :port port
